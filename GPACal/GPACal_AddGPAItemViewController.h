0aa69893d7b05b6da3e8e6b34563ab0312cf4f1a
Andrew Robinson <SirArkimedes@gmail.com>
2014-04-19 17:10:51 -0500

    Create class for AddGPAItem
    Add and connected the classes to View Controller of adding entries to
    the table.

---
+++ GPACal/GPACal_AddGPAItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  GPACal_AddGPAItemViewController.h
+//  GPACal
+//
+//  Created by Andrew Robinson on 4/19/14.
+//  Copyright (c) 2014 Andrew Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GPACal_AddGPAItemViewController : UIViewController
+
+@end
---
+++ GPACal/GPACal_AddGPAItemViewController.m
@@ -0,0 +1,49 @@
+//
+//  GPACal_AddGPAItemViewController.m
+//  GPACal
+//
+//  Created by Andrew Robinson on 4/19/14.
+//  Copyright (c) 2014 Andrew Robinson. All rights reserved.
+//
+
+#import "GPACal_AddGPAItemViewController.h"
+
+@interface GPACal_AddGPAItemViewController ()
+
+@end
+
+@implementation GPACal_AddGPAItemViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
