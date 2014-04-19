5af69ad56ad22179766926f09aa29e2534f9aa5b
Andrew Robinson <SirArkimedes@gmail.com>
2014-04-18 21:07:19 -0500

    Create Xcode project files
    Initialize empty application Xcode project.

---
+++ GPACal/GPACal_AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GPACal_AppDelegate.h
+//  GPACal
+//
+//  Created by Andrew Robinson on 4/18/14.
+//  Copyright (c) 2014 Andrew Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GPACal_AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GPACal/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GPACal
+//
+//  Created by Andrew Robinson on 4/18/14.
+//  Copyright (c) 2014 Andrew Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GPACal_AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GPACal_AppDelegate class]));
+    }
+}
