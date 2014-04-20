02836966a90565d7781fd95bcff66b766597b3ef
Andrew Robinson <SirArkimedes@gmail.com>
2014-04-19 23:46:31 -0500

    Create data storage classes
    Added the class used for data storage.

---
+++ GPACal/GPACal_GPAItem.m
@@ -0,0 +1,13 @@
+//
+//  GPACal_GPAItem.m
+//  GPACal
+//
+//  Created by Andrew Robinson on 4/19/14.
+//  Copyright (c) 2014 Andrew Robinson. All rights reserved.
+//
+
+#import "GPACal_GPAItem.h"
+
+@implementation GPACal_GPAItem
+
+@end
